4c481d9827bfbceb16c42313369793f25c3ea6f8
Diam Diallo <diamil.dev@gmail.com>
2024-11-13 15:52:41 +0000

    first commit

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Input } from '../ui/input'
+
+function NavSearch() {
+  return (
+    <Input
+      type='text'
+      placeholder='Find a property...'
+      className='max-w-xs dark:bg-muted'
+    />
+  )
+}
+
+export default NavSearch
---
+++ components/navbar/UserIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { LuUser2 } from 'react-icons/lu'
+
+function UserIcon() {
+  return <LuUser2 className='w-6 h-6 bg-primary text-white rounded-full' />
+}
+
+export default UserIcon
